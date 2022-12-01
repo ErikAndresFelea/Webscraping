eff9b4ef5d39ebf1133b7a330e13b65ee07e1003
ErikAndresFelea <erikandresfelea@gmail.com>
2022-12-01 13:00:22 +0100

    fixed redundancy

--- src/main.py
+++ src/main.py
@@ -239,9 +239,9 @@ if __name__ == '__main__':
     path = 'https://app.web3mba.io/'
 
     # Clear old data & store new data
-    file = open('src\\stored\\info_gathered.txt', 'w')
-    file.close()
-    file = open('src\\stored\\info_gathered.txt', 'w', encoding='utf-8')
+    # file = open('src\\stored\\info_gathered.txt', 'w')
+    # file.close()
+    file = open('src\\resources\\info_gathered.txt', 'w', encoding='utf-8')
 
     login(path)
     obtain_links()
