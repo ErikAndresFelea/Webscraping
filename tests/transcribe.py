e543a0a76d1fd1b7a77c0dfc924af4e7a867b3f9
Erik Andres Felea <erikandresfelea@gmail.com>
2022-12-09 10:20:20 +0100

    Transcribe attempt

--- tests/transcribe.py
+++ tests/transcribe.py
@@ -1,9 +1,13 @@
 import whisper
 
-model = whisper.load_model('base')
-result = model.transcribe('src\\courses\\prueba_transcribe.mp3')
-print(result["text"])
+audio_path = 'src\\resources\\JavaScript in 100 Seconds.mp3'
 
+model = whisper.load_model('small')
+result = model.transcribe(audio_path, fp16 = False, language = 'es')
+print(result['text'])
+
+
+''' Atempt to write the results on a file '''
 ### file = open('tests\\test_output.txt', 'w', encoding='utf-8')
 ### file.write(result['text'])
 ### file.close()
